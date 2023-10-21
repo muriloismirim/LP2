38b97eec9b34f35303c5235c9844f5c8523efaf6
muriloismirim <143854509+muriloismirim@users.noreply.github.com>
2023-10-20 22:11:46 -0300

    Add files via upload

---
+++ Atividade7/PMetodos/PMetodos/frmExercicio3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PMetodos
+{
+    public partial class frmExercicio3 : Form
+    {
+        public frmExercicio3()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRemover1_Click(object sender, EventArgs e)
+        {
+            int posicao = txtPalavra2.Text.IndexOf(txtPalavra1.Text,StringComparison.OrdinalIgnoreCase);
+            while(posicao>=0)
+            {
+                txtPalavra2.Text = txtPalavra2.Text.Substring(0, posicao) + 
+                    txtPalavra2.Text.Substring(posicao + txtPalavra1.Text.Length, 
+                    txtPalavra2.Text.Length - posicao - txtPalavra1.Text.Length);
+
+                posicao = txtPalavra2.Text.IndexOf(txtPalavra1.Text, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+
+        private void BtnRemover2_Click(object sender, EventArgs e)
+        {
+            txtPalavra1.Text = txtPalavra1.Text.ToUpper();
+            txtPalavra2.Text = txtPalavra2.Text.ToUpper();
+            txtPalavra2.Text = txtPalavra2.Text.Replace(txtPalavra1.Text, "");
+        }
+
+        private void BtnInverte_Click(object sender, EventArgs e)
+        {
+            // transformando string em array de char
+            char[] vetorC = txtPalavra1.Text.ToCharArray();
+            // inverte o array
+            Array.Reverse(vetorC);
+            txtPalavra2.Text = "";
+            //foreach (char C in vetorC)
+               // txtPalavra2.Text += vetorC;
+
+           txtPalavra2.Text = new string(vetorC);
+           // txtPalavra2.Text = vetorC.ToString();
+        }
+    }
+}
---
+++ Atividade7/PMetodos/PMetodos/frmExercicio5.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PMetodos
+{
+    partial class frmExercicio5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.BtnSorteio = new System.Windows.Forms.Button();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(107, 26);
+            this.txtNumero1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(187, 20);
+            this.txtNumero1.TabIndex = 0;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(107, 56);
+            this.txtNumero2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(187, 20);
+            this.txtNumero2.TabIndex = 1;
+            // 
+            // BtnSorteio
+            // 
+            this.BtnSorteio.Location = new System.Drawing.Point(234, 149);
+            this.BtnSorteio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnSorteio.Name = "BtnSorteio";
+            this.BtnSorteio.Size = new System.Drawing.Size(98, 42);
+            this.BtnSorteio.TabIndex = 2;
+            this.BtnSorteio.Text = "Sorteio";
+            this.BtnSorteio.UseVisualStyleBackColor = true;
+            this.BtnSorteio.Click += new System.EventHandler(this.BtnSorteio_Click);
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Location = new System.Drawing.Point(39, 29);
+            this.lblNumero1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(57, 13);
+            this.lblNumero1.TabIndex = 3;
+            this.lblNumero1.Text = "1° Número";
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Location = new System.Drawing.Point(39, 58);
+            this.lblNumero2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(57, 13);
+            this.lblNumero2.TabIndex = 4;
+            this.lblNumero2.Text = "2° Número";
+            // 
+            // frmExercicio5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.BtnSorteio);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmExercicio5";
+            this.Text = "frmExercicio5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button BtnSorteio;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+    }
+}
